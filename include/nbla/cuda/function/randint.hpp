6f66bdc46db6d0a628637513d5fe130840706f35
Takuya Narihira <Takuya.Narihira@jp.sony.com>
2017-07-13 08:09:44 +0900

    Add RandCuda, RandintCuda, RandnCuda

---
+++ include/nbla/cuda/function/randint.hpp
@@ -0,0 +1,63 @@
+// Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/** Randint
+ */
+#ifndef __NBLA_CUDA_FUNCTION_RANDINT_HPP__
+#define __NBLA_CUDA_FUNCTION_RANDINT_HPP__
+
+#include <nbla/cuda/common.hpp>
+#include <nbla/cuda/cuda.hpp>
+#include <nbla/cuda/utils/random.hpp>
+#include <nbla/function/randint.hpp>
+
+namespace nbla {
+/** @copydoc Randint
+*/
+
+template <typename T> class RandintCuda : public Randint<T> {
+public:
+  explicit RandintCuda(const Context &ctx, int low, int high,
+                       const vector<int> &shape, int seed)
+      : Randint<T>(ctx, low, high, shape, seed),
+        device_(std::stoi(ctx.device_id)) {
+    cuda_set_device(device_);
+    if (this->seed_ != -1) {
+      curand_generator_ = curand_create_generator(this->seed_);
+    } else {
+      curand_generator_ = SingletonManager::get<Cuda>()->curand_generator();
+    }
+  }
+  virtual ~RandintCuda() {
+    if (this->seed_ != -1) {
+      curand_destroy_generator(curand_generator_);
+    }
+  }
+  virtual string name() { return "RandintCuda"; }
+  virtual vector<string> allowed_array_classes() {
+    return SingletonManager::get<Cuda>()->array_classes();
+  }
+
+protected:
+  int device_;
+  curandGenerator_t curand_generator_;
+  virtual void setup_impl(const Variables &inputs, const Variables &outputs);
+  virtual void forward_impl(const Variables &inputs, const Variables &outputs);
+  virtual void backward_impl(const Variables &inputs, const Variables &outputs,
+                             const vector<bool> &propagate_down,
+                             const vector<bool> &accum);
+};
+}
+
+#endif
---
+++ include/nbla/cuda/function/randn.hpp
@@ -0,0 +1,62 @@
+// Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/** Randn
+ */
+#ifndef __NBLA_CUDA_FUNCTION_RANDN_HPP__
+#define __NBLA_CUDA_FUNCTION_RANDN_HPP__
+
+#include <nbla/cuda/common.hpp>
+#include <nbla/cuda/cuda.hpp>
+#include <nbla/cuda/utils/random.hpp>
+#include <nbla/function/randn.hpp>
+
+namespace nbla {
+/** @copydoc Randn
+*/
+
+template <typename T> class RandnCuda : public Randn<T> {
+public:
+  explicit RandnCuda(const Context &ctx, float mu, float sigma,
+                     const vector<int> &shape, int seed)
+      : Randn<T>(ctx, mu, sigma, shape, seed),
+        device_(std::stoi(ctx.device_id)) {
+    if (this->seed_ != -1) {
+      curand_generator_ = curand_create_generator(this->seed_);
+    } else {
+      curand_generator_ = SingletonManager::get<Cuda>()->curand_generator();
+    }
+  }
+  virtual ~RandnCuda() {
+    if (this->seed_ != -1) {
+      curand_destroy_generator(curand_generator_);
+    }
+  }
+  virtual string name() { return "RandnCuda"; }
+  virtual vector<string> allowed_array_classes() {
+    return SingletonManager::get<Cuda>()->array_classes();
+  }
+
+protected:
+  int device_;
+  curandGenerator_t curand_generator_;
+  virtual void setup_impl(const Variables &inputs, const Variables &outputs);
+  virtual void forward_impl(const Variables &inputs, const Variables &outputs);
+  virtual void backward_impl(const Variables &inputs, const Variables &outputs,
+                             const vector<bool> &propagate_down,
+                             const vector<bool> &accum);
+};
+}
+
+#endif
