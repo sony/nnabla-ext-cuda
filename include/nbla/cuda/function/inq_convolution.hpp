d5fd79c47fbb4ece3263156e85c161d1808ea3bb
Stefan Uhlich <stefan.uhlich@eu.sony.com>
2017-10-10 14:32:22 +0200

    Add CUDA implementation of `INQAffine` and `INQConvolution`

---
+++ include/nbla/cuda/function/inq_convolution.hpp
@@ -0,0 +1,63 @@
+// Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/** INQConvolution
+ */
+#ifndef __NBLA_CUDA_FUNCTION_INQCONVOLUTION_HPP__
+#define __NBLA_CUDA_FUNCTION_INQCONVOLUTION_HPP__
+
+#include <nbla/cuda/common.hpp>
+#include <nbla/cuda/cuda.hpp>
+#include <nbla/cuda/utils/random.hpp>
+#include <nbla/function/inq_convolution.hpp>
+namespace nbla {
+/** @copydoc INQConvolution
+*/
+
+template <typename T, typename T1>
+class INQConvolutionCuda : public INQConvolution<T, T1> {
+public:
+  explicit INQConvolutionCuda(const Context &ctx, int base_axis,
+                              const vector<int> &pad, const vector<int> &stride,
+                              const vector<int> &dilation, int group,
+                              int num_bits, const vector<int> &inq_iterations,
+                              const string &selection_algorithm, int seed)
+      : INQConvolution<T, T1>(ctx, base_axis, pad, stride, dilation, group,
+                              num_bits, inq_iterations, selection_algorithm,
+                              seed),
+        device_(std::stoi(ctx.device_id)) {}
+  virtual ~INQConvolutionCuda() {
+    if (this->selection_algorithm_ == "random" && this->seed_ != -1) {
+      curand_destroy_generator(curand_generator_);
+    }
+  }
+  virtual string name() { return "INQConvolutionCuda"; }
+  virtual vector<string> allowed_array_classes() {
+    return SingletonManager::get<Cuda>()->array_classes();
+  }
+
+protected:
+  int device_;
+  curandGenerator_t curand_generator_;
+  Variable indices_;
+  Variable cumulative_count_;
+  virtual void setup_impl(const Variables &inputs, const Variables &outputs);
+  virtual void forward_impl(const Variables &inputs, const Variables &outputs);
+  virtual void backward_impl(const Variables &inputs, const Variables &outputs,
+                             const vector<bool> &propagate_down,
+                             const vector<bool> &accum);
+};
+}
+
+#endif
