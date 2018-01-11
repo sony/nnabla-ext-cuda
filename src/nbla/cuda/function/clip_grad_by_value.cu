cdd9b95c3191e1c095f10a6971d76e21d0080ce9
KazukiYoshiyama <Kazuki.Yoshiyama@jp.sony.com>
2018-01-11 14:22:31 +0900

    Add clip_by_value.
    Make it easier to read.
    
    Change interfaces and implementations.
    
    Format

---
+++ src/nbla/cuda/function/clip_grad_by_value.cu
@@ -0,0 +1,94 @@
+// Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <nbla/array.hpp>
+#include <nbla/cuda/common.hpp>
+#include <nbla/cuda/function/clip_grad_by_value.hpp>
+#include <nbla/cuda/math.hpp>
+#include <nbla/variable.hpp>
+
+namespace nbla {
+
+template <typename T>
+__global__ void kernel_clip_grad_by_value_forward(const int num, T *y,
+                                                  const T *x) {
+  NBLA_CUDA_KERNEL_LOOP(idx, num) { y[idx] = x[idx]; }
+}
+
+template <typename T, bool accum = true>
+__global__ void kernel_clip_grad_by_value_backward(const int num, T *dx,
+                                                   const T *dy, const T *min,
+                                                   const T *max) {
+  NBLA_CUDA_KERNEL_LOOP(idx, num) {
+    T min_i = min[idx];
+    T max_i = max[idx];
+    if (accum) {
+      if (dy[idx] > max_i) {
+        dx[idx] += max_i;
+      } else if (dy[idx] < min_i) {
+        dx[idx] += min_i;
+      } else {
+        dx[idx] += dy[idx];
+      }
+    } else {
+      if (dy[idx] > max_i) {
+        dx[idx] = max_i;
+      } else if (dy[idx] < min_i) {
+        dx[idx] = min_i;
+      } else {
+        dx[idx] = dy[idx];
+      }
+    }
+  }
+}
+
+template <typename T>
+void ClipGradByValueCuda<T>::forward_impl(const Variables &inputs,
+                                          const Variables &outputs) {
+  cuda_set_device(this->device_);
+  Size_t size = inputs[0]->size();
+  const T *x = inputs[0]->get_data_pointer<T>(this->ctx_);
+  T *y = outputs[0]->cast_data_and_get_pointer<T>(this->ctx_);
+  NBLA_CUDA_LAUNCH_KERNEL_SIMPLE(kernel_clip_grad_by_value_forward, size, y, x);
+}
+
+template <typename T>
+void ClipGradByValueCuda<T>::backward_impl(const Variables &inputs,
+                                           const Variables &outputs,
+                                           const vector<bool> &propagate_down,
+                                           const vector<bool> &accum) {
+  cuda_set_device(this->device_);
+  // No backward to min and max varialbes.
+  if (!propagate_down[0]) {
+    return;
+  }
+
+  Size_t size = inputs[0]->size();
+  T *dx = inputs[0]->cast_grad_and_get_pointer<T>(this->ctx_);
+  const T *dy = outputs[0]->get_grad_pointer<T>(this->ctx_);
+  const T *min = inputs[1]->get_data_pointer<T>(this->ctx_);
+  const T *max = inputs[2]->get_data_pointer<T>(this->ctx_);
+
+  if (accum[0]) {
+    NBLA_CUDA_LAUNCH_KERNEL_SIMPLE(
+        (kernel_clip_grad_by_value_backward<T, true>), size, dx, dy, min, max);
+  } else {
+    NBLA_CUDA_LAUNCH_KERNEL_SIMPLE(
+        (kernel_clip_grad_by_value_backward<T, false>), size, dx, dy, min, max);
+  }
+}
+
+// template instantiation
+template class ClipGradByValueCuda<float>;
+}
