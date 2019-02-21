9dd1be219cb8628763c40b81dcc796103f29aab0
NNabla developer <nnabla@example.com>
2019-02-21 21:36:06 +0900

    auto-format

--- include/nbla/cuda/function/gelu.hpp
+++ include/nbla/cuda/function/gelu.hpp
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
-
 #ifndef NBLA_CUDA_FUNCTION_GELU_HPP
 #define NBLA_CUDA_FUNCTION_GELU_HPP
 
