de256032d048654a52c512885af283f8e4932b54
Takuya Narihira <Takuya.Narihira@sony.com>
2019-02-22 23:06:51 +0900

    [C++] Fix: CUDA error state was not cleared

--- include/nbla/cuda/common.hpp
+++ include/nbla/cuda/common.hpp
@@ -47,13 +47,16 @@ using std::map;
 
 /**
 Check CUDA error for synchronous call
+cudaGetLastError is used to clear previous error happening at "condition".
 */
 #define NBLA_CUDA_CHECK(condition)                                             \
   {                                                                            \
     cudaError_t error = condition;                                             \
     if (error != cudaSuccess) {                                                \
-      NBLA_ERROR(error_code::target_specific, "(%s) failed with \"%s\".",      \
-                 #condition, cudaGetErrorString(error));                       \
+      cudaGetLastError();                                                      \
+      NBLA_ERROR(error_code::target_specific, "(%s) failed with \"%s\" (%s).", \
+                 #condition, cudaGetErrorString(error),                        \
+                 cudaGetErrorName(error));                                     \
     }                                                                          \
   }
 
