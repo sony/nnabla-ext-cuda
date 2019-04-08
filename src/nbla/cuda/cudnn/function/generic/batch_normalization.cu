bd51d8b5127dc63ebcf4fc782d7c194c1ae6ac82
Takuya Narihira <Takuya.Narihira@sony.com>
2019-04-08 11:36:56 +0900

    NHWC BatchNormalization

--- src/nbla/cuda/cudnn/function/generic/batch_normalization.cu
+++ src/nbla/cuda/cudnn/function/generic/batch_normalization.cu
@@ -22,6 +22,8 @@
 #include <nbla/cuda/function/batch_normalization.hpp>
 #include <nbla/cuda/limits.hpp>
 
+#include <type_traits>
+
 namespace nbla {
 
 #define DRV_BN_T() get_dtype_by_cudnn_data_type(derived_bn_dtype_)
@@ -29,22 +31,89 @@ namespace nbla {
 template <typename T>
 void BatchNormalizationCudaCudnn<T>::setup_impl(const Variables &inputs,
                                                 const Variables &outputs) {
+  if (outputs.size() == 3) {
+    // [WORKAROUND]
+    // To use saved mean and variance and to propagate mean and variance
+    // gradient are not supported with cuDNN.
+    // Because cuDNN's backward interface is different from NNabla's one.
+    // So Fall back to CUDA implementation if outputs.size() == 3
+    // TODO: Change saved variance to inverse variance like cuDNN
+    this->fall_back_func_ = make_shared<BatchNormalizationCuda<T>>(
+        this->ctx_, this->axes_, this->decay_rate_, this->eps_,
+        this->batch_stat_);
+    this->fall_back_func_->setup(inputs, outputs);
+    return;
+  }
   BatchNormalizationCuda<T>::setup_impl(inputs, outputs);
   cudnn_handle_ = SingletonManager::get<CudnnHandleManager>()->handle(device_);
+  NBLA_CHECK(this->axes_.size() == 1, error_code::value,
+             "Axes on a single dimension is only supported.");
   int N = this->size0_;
   int C = this->size1_;
   int H = this->size2_;
   int W = 1;
-  NBLA_CUDNN_CHECK(cudnnSetTensor4dDescriptor(
-      input_desc_, CUDNN_TENSOR_NCHW, cudnn_data_type<T>::type(), N, C, H, W));
-  NBLA_CUDNN_CHECK(cudnnSetTensor4dDescriptor(
-      output_desc_, CUDNN_TENSOR_NCHW, cudnn_data_type<T>::type(), N, C, H, W));
-  NBLA_CUDNN_CHECK(cudnnDeriveBNTensorDescriptor(bn_scale_bias_mean_var_desc_,
-                                                 input_desc_, mode_));
+  mode_ = CUDNN_BATCHNORM_SPATIAL;
+  bool channel_last = this->axes_[0] == inputs[0]->ndim() - 1;
+  if (channel_last) {
+    // To prevent NOT SUPPORTED error in CUDNNN, N and H are recalculated.
+    // (Large N is not allowed.)
+    N = inputs[0]->shape()[0];
+    H = inputs[0]->size() / (N * C);
+    if (this->batch_stat_) {
+      // cudnnBatchNormalizationForwardInference does not support this mode.
+      mode_ = CUDNN_BATCHNORM_SPATIAL_PERSISTENT;
+    }
+    NBLA_CUDNN_CHECK(
+        cudnnSetTensor4dDescriptor(input_desc_.desc, CUDNN_TENSOR_NHWC,
+                                   cudnn_data_type<T>::type(), N, C, H, W));
+    NBLA_CUDNN_CHECK(
+        cudnnSetTensor4dDescriptor(output_desc_.desc, CUDNN_TENSOR_NHWC,
+                                   cudnn_data_type<T>::type(), N, C, H, W));
+  } else {
+    NBLA_CUDNN_CHECK(
+        cudnnSetTensor4dDescriptor(input_desc_.desc, CUDNN_TENSOR_NCHW,
+                                   cudnn_data_type<T>::type(), N, C, H, W));
+    NBLA_CUDNN_CHECK(
+        cudnnSetTensor4dDescriptor(output_desc_.desc, CUDNN_TENSOR_NCHW,
+                                   cudnn_data_type<T>::type(), N, C, H, W));
+  }
+
+  // Get BN data type.
+  NBLA_CUDNN_CHECK(cudnnDeriveBNTensorDescriptor(
+      bn_scale_bias_mean_var_desc_.desc, input_desc_.desc, mode_));
   int n, c, h, w, sn, sc, sh, sw; // garbage
-  NBLA_CUDNN_CHECK(cudnnGetTensor4dDescriptor(bn_scale_bias_mean_var_desc_,
+  NBLA_CUDNN_CHECK(cudnnGetTensor4dDescriptor(bn_scale_bias_mean_var_desc_.desc,
                                               &derived_bn_dtype_, &n, &c, &h,
                                               &w, &sn, &sc, &sh, &sw));
+#if CUDNN_VERSION >= 7400
+  // Check if the confition we can use faster BN.
+  can_use_bn_ex_ =
+      channel_last && std::is_same<Tw, nbla::HalfCuda>::value && C % 4 == 0;
+  can_use_bn_ex_ &= this->batch_stat_;
+  if (can_use_bn_ex_) {
+    NBLA_CUDNN_CHECK(cudnnGetBatchNormalizationForwardTrainingExWorkspaceSize(
+        this->cudnn_handle_, this->mode_, this->ops_,
+        this->input_desc_.desc,  /* x desc */
+        nullptr,                 /* z desc */
+        this->output_desc_.desc, /* y desc */
+        this->bn_scale_bias_mean_var_desc_.desc, nullptr,
+        &forward_workspace_size_));
+
+    NBLA_CUDNN_CHECK(cudnnGetBatchNormalizationTrainingExReserveSpaceSize(
+        this->cudnn_handle_, this->mode_, this->ops_, this->act_desc_.desc,
+        this->input_desc_.desc, &reserve_size_));
+
+    NBLA_CUDNN_CHECK(cudnnGetBatchNormalizationBackwardExWorkspaceSize(
+        this->cudnn_handle_, this->mode_, this->ops_,
+        this->input_desc_.desc,  /* x desc */
+        this->output_desc_.desc, /* y desc */
+        this->output_desc_.desc, /* dy desc */
+        this->input_desc_.desc,  /*dz desc*/
+        this->input_desc_.desc,  /* dx desc */
+        this->bn_scale_bias_mean_var_desc_.desc, this->act_desc_.desc,
+        &backward_workspace_size_));
+  }
+#endif
 }
 
 template <class T>
@@ -61,24 +130,16 @@ void BatchNormalizationCudaCudnn<T>::forward_impl(const Variables &inputs,
 template <class T>
 void BatchNormalizationCudaCudnn<T>::forward_impl_batch(
     const Variables &inputs, const Variables &outputs) {
-
   // Check whether it outputs batch mean and var.
   Variable *batch_mean = &this->mean_;
   Variable *batch_var = &this->var_;
-  if (outputs.size() == 3) {
-    // [WORKAROUND]
-    // To save mean and variance is not supported with cuDNN.
-    // Because cuDNN's forward training interface is different from NNabla's
-    // one.
-    // So Fall back to CUDA implementation if outputs.size() == 3
-    // TODO: Change saved variance to inverse variance like cuDNN
-    BatchNormalizationCuda<T>::forward_impl_batch(inputs, outputs);
-    return;
-  }
+
   // Inputs
   const Tw *x = inputs[0]->get_data_pointer<Tw>(this->ctx_);
+
   const void *beta =
       inputs[1]->data()->get(DRV_BN_T(), this->ctx_)->const_pointer();
+
   const void *gamma =
       inputs[2]->data()->get(DRV_BN_T(), this->ctx_)->const_pointer();
 
@@ -97,19 +158,45 @@ void BatchNormalizationCudaCudnn<T>::forward_impl_batch(
                  ->pointer(); // running mean
   void *rv =
       inputs[4]->data()->cast(DRV_BN_T(), this->ctx_)->pointer(); // running var
+
   auto a = get_cudnn_scalar_arg<T>(1);
   auto b = get_cudnn_scalar_arg<T>(0);
+  double eps = std::max((double)this->eps_, CUDNN_BN_MIN_EPSILON);
+#if CUDNN_VERSION >= 7400
+  if (can_use_bn_ex_) {
+    // Get buffers.
+    NdArray workspace(Shape_t{(Size_t)forward_workspace_size_});
+    reserve_ = make_shared<NdArray>(Shape_t{(Size_t)reserve_size_});
+    void *workspace_ptr =
+        workspace.cast(DRV_BN_T(), this->ctx_, true)->pointer();
+    void *reserve_ptr = reserve_->cast(DRV_BN_T(), this->ctx_, true)->pointer();
+    // Execute forward.
+    NBLA_CUDNN_CHECK(cudnnBatchNormalizationForwardTrainingEx(
+        this->cudnn_handle_, this->mode_, this->ops_, &a, &b, input_desc_.desc,
+        x,                    /* x */
+        nullptr, nullptr,     /* z */
+        output_desc_.desc, y, /* y */
+        this->bn_scale_bias_mean_var_desc_.desc, gamma, beta,
+        1 - this->decay_rate_, rm, rv, eps, m, v,
+        this->act_desc_.desc,    /* activation descriptor */
+        workspace_ptr,           /* workspace pointer */
+        forward_workspace_size_, /* workspace size */
+        reserve_ptr,             /* reserve space pointer */
+        reserve_size_            /* reserve space size */
+        ));
+    return;
+  }
+#endif
   NBLA_CUDNN_CHECK(cudnnBatchNormalizationForwardTraining(
-      cudnn_handle_, mode_, &a, &b, input_desc_, x, output_desc_, y,
-      bn_scale_bias_mean_var_desc_, gamma, beta, 1 - this->decay_rate_, rm, rv,
-      epsilon, m, v));
+      cudnn_handle_, mode_, &a, &b, input_desc_.desc, x, output_desc_.desc, y,
+      bn_scale_bias_mean_var_desc_.desc, gamma, beta, 1 - this->decay_rate_, rm,
+      rv, eps, m, v));
 }
 
 template <class T>
 void BatchNormalizationCudaCudnn<T>::forward_impl_global(
     const Variables &inputs, const Variables &outputs) {
   // Inputs
-  // while(1);
   const Tw *x = inputs[0]->get_data_pointer<Tw>(this->ctx_);
   const void *beta =
       inputs[1]->data()->get(DRV_BN_T(), this->ctx_)->const_pointer();
@@ -128,10 +215,10 @@ void BatchNormalizationCudaCudnn<T>::forward_impl_global(
 
   auto a = get_cudnn_scalar_arg<T>(1);
   auto b = get_cudnn_scalar_arg<T>(0);
-  double epsilon = max(this->eps_, CUDNN_BN_MIN_EPSILON);
+  double eps = std::max((double)this->eps_, CUDNN_BN_MIN_EPSILON);
   NBLA_CUDNN_CHECK(cudnnBatchNormalizationForwardInference(
-      cudnn_handle_, mode_, &a, &b, input_desc_, x, output_desc_, y,
-      bn_scale_bias_mean_var_desc_, gamma, beta, rm, rv, epsilon));
+      cudnn_handle_, mode_, &a, &b, input_desc_.desc, x, output_desc_.desc, y,
+      bn_scale_bias_mean_var_desc_.desc, gamma, beta, rm, rv, eps));
 }
 
 template <class T>
@@ -157,17 +244,6 @@ void BatchNormalizationCudaCudnn<T>::backward_impl_batch(
   // Check whether it outputs batch mean/var.
   Variable *batch_mean = &this->mean_;
   Variable *batch_var = &this->var_;
-  if (outputs.size() == 3) {
-    // [WORKAROUND]
-    // To use saved mean and variance and to propagate mean and variance
-    // gradient are not supported with cuDNN.
-    // Because cuDNN's backward interface is different from NNabla's one.
-    // So Fall back to CUDA implementation if outputs.size() == 3
-    // TODO: Change saved variance to inverse variance like cuDNN
-    BatchNormalizationCuda<T>::backward_impl_batch(inputs, outputs,
-                                                   propagate_down, accum);
-    return;
-  }
   // Common inputs wrt. gradient.
   const Tw *dy = outputs[0]->get_grad_pointer<Tw>(this->ctx_);
   const void *m =
@@ -176,58 +252,90 @@ void BatchNormalizationCudaCudnn<T>::backward_impl_batch(
       batch_var->data()->get(DRV_BN_T(), this->ctx_)->const_pointer();
   const Tw *x = inputs[0]->get_data_pointer<Tw>(this->ctx_);
 
-  if (propagate_down[0] || propagate_down[1] || propagate_down[2]) {
-    auto a_data = get_cudnn_scalar_arg<T>(propagate_down[0] ? 1 : 0);
-    auto b_data =
-        get_cudnn_scalar_arg<T>(accum[0] && propagate_down[0] ? 1 : 0);
-    auto a_param =
-        get_cudnn_scalar_arg<T>(propagate_down[1] || propagate_down[2] ? 1 : 0);
-    auto b_param = a_param;
-    if (!(accum[1] || accum[2])) {
-      b_param = 0;
-    } else {
-      if (!accum[1])
-        inputs[1]->grad()->zero();
-      if (!accum[2])
-        inputs[2]->grad()->zero();
-    }
-
-    size_t workspace_size = 0;
-    if (!propagate_down[0]) {
-      workspace_size = std::max(workspace_size,
-                                inputs[0]->size() * sizeof_dtype(DRV_BN_T()));
-    }
-    if (!propagate_down[1] || !propagate_down[2]) {
-      workspace_size = std::max(workspace_size,
-                                inputs[1]->size() * sizeof_dtype(DRV_BN_T()));
-    }
-    void *tmp_buf = nullptr;
-    shared_ptr<CudaCachedArray> mem_workspace(
-        workspace_size
-            ? new CudaCachedArray(workspace_size, dtypes::BYTE, this->ctx_)
-            : nullptr);
-    if (workspace_size) {
-      tmp_buf = mem_workspace->pointer();
-    }
-
-    Tw *dx =
-        propagate_down[0]
-            ? inputs[0]->cast_grad_and_get_pointer<Tw>(this->ctx_, !accum[0])
-            : (Tw *)tmp_buf;
-    const void *gamma =
-        inputs[2]->data()->get(DRV_BN_T(), this->ctx_)->const_pointer();
-    void *db =
-        propagate_down[1]
-            ? inputs[1]->grad()->cast(DRV_BN_T(), this->ctx_, false)->pointer()
-            : tmp_buf;
-    void *dg =
-        propagate_down[2]
-            ? inputs[2]->grad()->cast(DRV_BN_T(), this->ctx_, false)->pointer()
-            : tmp_buf;
-    NBLA_CUDNN_CHECK(cudnnBatchNormalizationBackward(
-        cudnn_handle_, mode_, &a_data, &b_data, &a_param, &b_param, input_desc_,
-        x, output_desc_, dy, input_desc_, dx, bn_scale_bias_mean_var_desc_,
-        gamma, dg, db, epsilon, m, v));
+  auto a_data = get_cudnn_scalar_arg<T>(propagate_down[0] ? 1 : 0);
+  auto b_data = get_cudnn_scalar_arg<T>(accum[0] && propagate_down[0] ? 1 : 0);
+  auto a_param =
+      get_cudnn_scalar_arg<T>(propagate_down[1] || propagate_down[2] ? 1 : 0);
+  auto b_param = a_param;
+  if (!(accum[1] || accum[2])) {
+    b_param = 0;
+  } else {
+    if (!accum[1])
+      inputs[1]->grad()->zero();
+    if (!accum[2])
+      inputs[2]->grad()->zero();
   }
+
+  size_t prop_down_workspace_size = 0;
+  if (!propagate_down[0]) {
+    prop_down_workspace_size = std::max(
+        prop_down_workspace_size, inputs[0]->size() * sizeof_dtype(DRV_BN_T()));
+  }
+  if (!propagate_down[1] || !propagate_down[2]) {
+    prop_down_workspace_size = std::max(
+        prop_down_workspace_size, inputs[1]->size() * sizeof_dtype(DRV_BN_T()));
+  }
+  void *prop_down_buf = nullptr;
+  shared_ptr<CudaCachedArray> prop_down_workspace(
+      prop_down_workspace_size ? new CudaCachedArray(prop_down_workspace_size,
+                                                     dtypes::BYTE, this->ctx_)
+                               : nullptr);
+  if (prop_down_workspace_size) {
+    prop_down_buf = prop_down_workspace->pointer();
+  }
+
+  Tw *dx = propagate_down[0]
+               ? inputs[0]->cast_grad_and_get_pointer<Tw>(this->ctx_, !accum[0])
+               : (Tw *)prop_down_buf;
+
+  const void *beta =
+      inputs[1]->data()->get(DRV_BN_T(), this->ctx_)->const_pointer();
+
+  const void *gamma =
+      inputs[2]->data()->get(DRV_BN_T(), this->ctx_)->const_pointer();
+
+  void *db =
+      propagate_down[1]
+          ? inputs[1]->grad()->cast(DRV_BN_T(), this->ctx_, false)->pointer()
+          : prop_down_buf;
+  void *dg =
+      propagate_down[2]
+          ? inputs[2]->grad()->cast(DRV_BN_T(), this->ctx_, false)->pointer()
+          : prop_down_buf;
+  double eps = std::max((double)this->eps_, CUDNN_BN_MIN_EPSILON);
+#if CUDNN_VERSION >= 7400
+  if (can_use_bn_ex_) {
+    // Get buffers.
+    NdArray workspace(Shape_t{(Size_t)backward_workspace_size_});
+    NBLA_CHECK(reserve_, error_code::value, "Forward is not called.");
+    void *workspace_ptr =
+        workspace.cast(DRV_BN_T(), this->ctx_, true)->pointer();
+    void *reserve_ptr =
+        reserve_->cast(DRV_BN_T(), this->ctx_, false /* rw access */)
+            ->pointer();
+    // Execute backward.
+    NBLA_CUDNN_CHECK(cudnnBatchNormalizationBackwardEx(
+        this->cudnn_handle_, this->mode_, this->ops_, &a_data, &b_data,
+        &a_param, &b_param, input_desc_.desc, x, /* x */
+        nullptr, nullptr,                        /* y */
+        output_desc_.desc, dy,                   /* dy */
+        nullptr, nullptr,                        /* dz == null */
+        input_desc_.desc, dx,                    /* dx */
+        this->bn_scale_bias_mean_var_desc_.desc, gamma, beta, dg, db, eps, m, v,
+        this->act_desc_.desc,     /* activation descriptor */
+        workspace_ptr,            /* workspace pointer */
+        backward_workspace_size_, /* workspace size */
+        reserve_ptr,              /* reserve space pointer */
+        reserve_size_             /* reserve space size */
+        ));
+    // Clear reserved buffer for backward
+    reserve_ = nullptr;
+    return;
+  }
+#endif
+  NBLA_CUDNN_CHECK(cudnnBatchNormalizationBackward(
+      cudnn_handle_, mode_, &a_data, &b_data, &a_param, &b_param,
+      input_desc_.desc, x, output_desc_.desc, dy, input_desc_.desc, dx,
+      bn_scale_bias_mean_var_desc_.desc, gamma, dg, db, eps, m, v));
 }
-}
+} // namespace nbla
