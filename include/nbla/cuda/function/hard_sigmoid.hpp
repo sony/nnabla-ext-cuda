dc586a32af7f05d42dec109ad577b4a639dd01b1
andrewshinsony <andrew.shin@sony.com>
2019-04-22 09:23:21 +0000

    built

---
+++ include/nbla/cuda/function/hard_sigmoid.hpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NBLA_CUDA_FUNCTION_HARDSIGMOID_HPP
+#define NBLA_CUDA_FUNCTION_HARDSIGMOID_HPP
+
+#include <nbla/cuda/function/utils/base_transform_unary.hpp>
+#include <nbla/function/hard_sigmoid.hpp>
+
+namespace nbla {
+
+/** @copydoc HardSigmoid
+*/
+NBLA_DECLARE_TRANSFORM_UNARY_CUDA(HardSigmoid);
+}
+#endif
