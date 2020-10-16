2ae4085796fc5c08f3b3f9286d8f1bf5a2375b10
TE-andrewshin <33474392+TE-andrewshin@users.noreply.github.com>
2020-10-16 11:27:08 +0900

    Add RMSpropGraves (#248)
    * Add Alex Graves's RMS prop solver
    
    * Fix formatting errors
    
    * Rename RMSpropgraves solver.
    
    Renamed as follows:
    RMSpropgraves -> RMSpropGraves. rmspropgraves -> rmsprop_graves
    
    Co-authored-by: Yu Ishihara <yu.ishihara@sony.com>

---
+++ include/nbla/cuda/solver/rmsprop_graves.hpp
@@ -0,0 +1,44 @@
+// Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef __NBLA_CUDA_SOLVER_RMSPROPGRAVES_HPP__
+#define __NBLA_CUDA_SOLVER_RMSPROPGRAVES_HPP__
+
+#include <nbla/cuda/cuda.hpp>
+#include <nbla/solver/rmsprop_graves.hpp>
+
+namespace nbla {
+
+template <typename T> class RMSpropGravesCuda : public RMSpropGraves<T> {
+public:
+  explicit RMSpropGravesCuda(const Context &ctx, float lr, float decay,
+                             float momentum, float eps)
+      : RMSpropGraves<T>(ctx, lr, decay, momentum, eps) {}
+  virtual ~RMSpropGravesCuda() {}
+  virtual string name() { return "RMSpropGravesCuda"; }
+  virtual vector<string> allowed_array_classes() {
+    return SingletonManager::get<Cuda>()->array_classes();
+  }
+
+protected:
+  virtual void update_impl(const string &key, VariablePtr param);
+  NBLA_DECL_WEIGHT_DECAY();
+  NBLA_DECL_CLIP_GRAD_BY_NORM();
+  NBLA_DECL_CHECK_INF_GRAD();
+  NBLA_DECL_CHECK_NAN_GRAD();
+  NBLA_DECL_CHECK_INF_OR_NAN_GRAD();
+  NBLA_DECL_SCALE_GRAD();
+};
+}
+#endif
