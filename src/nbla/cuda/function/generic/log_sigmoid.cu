137f1fe8bcf0975094b29199c3f2559ccb01bcbc
andrewshinsony <andrew.shin@sony.com>
2021-03-25 16:51:28 +0900

    Fix numerical instability for log_sigmoid and softplus

--- src/nbla/cuda/function/generic/log_sigmoid.cu
+++ src/nbla/cuda/function/generic/log_sigmoid.cu
@@ -22,6 +22,8 @@
 #include <cmath>
 
 namespace nbla {
-NBLA_DEFINE_TRANSFORM_UNARY_CUDA(LogSigmoid, -std::log(std::exp(-x) + (T)1),
+NBLA_DEFINE_TRANSFORM_UNARY_CUDA(LogSigmoid,
+                                 x > (T)0 ? -std::log(std::exp(-x) + (T)1)
+                                          : x - std::log(std::exp(x) + (T)1),
                                  dy / (std::exp(x) + (T)1), false);
 }
