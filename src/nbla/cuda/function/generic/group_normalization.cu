6c3c5f7a840d0dfd9419853cc4f5b3132c7f6b7c
Stephen Tiedemann <stephen.tiedemann@sony.com>
2021-10-25 13:11:05 +0200

    Need explicit Shape_t initialization since CUDA 11.1

--- src/nbla/cuda/function/generic/group_normalization.cu
+++ src/nbla/cuda/function/generic/group_normalization.cu
@@ -66,19 +66,19 @@ void GroupNormalizationCuda<T>::setup_impl(const Variables &inputs,
   //----------------
 
   // Batch stats
-  var_.reshape({batch_size_ * channel_size_}, true);
-  mean_.reshape({batch_size_ * channel_size_}, true);
+  var_.reshape(Shape_t{batch_size_ * channel_size_}, true);
+  mean_.reshape(Shape_t{batch_size_ * channel_size_}, true);
 
   // Internal buffers for forward calculation
-  a_.reshape({batch_size_ * channel_size_}, true);
-  b_.reshape({batch_size_ * channel_size_}, true);
+  a_.reshape(Shape_t{batch_size_ * channel_size_}, true);
+  b_.reshape(Shape_t{batch_size_ * channel_size_}, true);
 
   // Internal buffers for backward calculation
-  sum_dy_.reshape({batch_size_ * channel_size_}, true);
-  sum_dyx_.reshape({batch_size_ * channel_size_}, true);
-  gamma_invstd_.reshape({batch_size_ * channel_size_}, true);
-  factor1_.reshape({batch_size_ * this->num_groups_}, true);
-  factor2_.reshape({batch_size_ * this->num_groups_}, true);
+  sum_dy_.reshape(Shape_t{batch_size_ * channel_size_}, true);
+  sum_dyx_.reshape(Shape_t{batch_size_ * channel_size_}, true);
+  gamma_invstd_.reshape(Shape_t{batch_size_ * channel_size_}, true);
+  factor1_.reshape(Shape_t{batch_size_ * this->num_groups_}, true);
+  factor2_.reshape(Shape_t{batch_size_ * this->num_groups_}, true);
 }
 
 template <typename T>
