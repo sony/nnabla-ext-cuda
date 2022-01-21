15bb023da93108a8e807779e3eec68cb309fc698
woody.li <woody.li@sony.com>
2022-01-21 17:18:55 +0800

    update copyright -2

--- src/nbla/cuda/function/generic/cumsum.cu
+++ src/nbla/cuda/function/generic/cumsum.cu
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+// Copyright 2021 Sony Corporation.
 // Copyright 2021 Sony Group Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
