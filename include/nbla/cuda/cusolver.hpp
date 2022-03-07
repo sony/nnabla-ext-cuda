912b4f223a968d65086cefa4cfb0af96ede143e3
Yu Ishihara <yu.ishihara@sony.com>
2022-03-07 17:58:48 +0900

    Fix copyright

--- include/nbla/cuda/cusolver.hpp
+++ include/nbla/cuda/cusolver.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Sony Corporation. All Rights Reserved.
+// Copyright 2021 Sony Group Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#ifndef __NBLA_CUSOLVER_HPP__
-#define __NBLA_CUSOLVER_HPP__
+#ifndef NBLA_CUSOLVER_HPP
+#define NBLA_CUSOLVER_HPP
 
 #include <cusolverDn.h>
 
