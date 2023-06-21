275e2e2c5a49bccc995d42ba603579261cd0aee7
Qizhen.Xue <Qizhen.Xue@sony.com>
2023-06-21 20:36:49 +0800

    spell error and missing mpi env

--- docker/release/20-nvidia-cuda-compat.sh
+++ docker/release/20-nvidia-cuda-compat.sh
@@ -14,3 +14,15 @@
 # limitations under the License.
 
 test -f /etc/shinit_v2 && . /etc/shinit_v2
+
+if [ -d "/opt/mpi/hpcx-v2.12" ]; then
+    curdir=$PWD
+    cd /opt/mpi/hpcx-v2.12
+    . ./hpcx-init.sh
+    hpcx_load
+    cd $curdir
+    unset curdir
+else
+    export PATH=/opt/mpi/bin:$PATH
+    export LD_LIBRARY_PATH=/opt/mpi/lib:$LD_LIBRARY_PATH
+fi
