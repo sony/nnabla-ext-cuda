0b0f4ae0440f7e727a6079767211ce37570980f4
Qizhen.Xue <Qizhen.Xue@sony.com>
2023-06-28 17:20:58 +0800

    replace openmpi 4.1.3 with hpcx 2.12
    add missing curl opts

---
+++ docker/development/.entrypoint-hpcx.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Copyright 2023 Sony Group Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ -d "/opt/mpi/hpcx-v2.12" ]; then
+    curdir=$PWD
+    cd /opt/mpi/hpcx-v2.12
+    . ./hpcx-init.sh
+    hpcx_load
+    cd $curdir
+    unset curdir
+fi
+
+if [ $# -eq 0 ]; then
+    exec "/bin/bash"
+else
+    exec "$@"
+fi
